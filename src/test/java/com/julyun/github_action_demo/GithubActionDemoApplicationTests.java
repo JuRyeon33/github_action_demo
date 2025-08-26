0e2ea0a5135f4c2a484d5e076fee3c97b29343d9
JuRyeon33 <thwnfus0330@gmail.com>
2025-08-26 15:10:50 +0900

    first commit

---
+++ src/test/java/com/julyun/github_action_demo/GithubActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julyun.github_action_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
